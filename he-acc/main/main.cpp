cae7e0e1310bc06f07a9b42d669a98808f3526ed
nima <nima@sgx-bench-vm.ihgddfig0ewupkoegwlfb1yuif.gx.internal.cloudapp.net>
2025-06-09 10:52:33 +0000

    fix main file

--- he-acc/main/main.cpp
+++ he-acc/main/main.cpp
@@ -21,35 +21,35 @@ const std::string RESULTSFOLDER = "results";
 const std::string CRYPTOCONTEXT = "cryptocontext";
 
 
-// std::tuple<int, int, int> loadConfigParameters(const std::string& configFile = "data/config_params.txt") {
-//     std::ifstream inFile(configFile);
-//     if (!inFile.is_open()) {
-//         std::cerr << "Error: Could not open configuration file for reading: " << configFile << std::endl;
-//         return {8, 65537, 128}; // Return default values
-//     }
+std::tuple<int, int, int> loadConfigParameters(const std::string& configFile = DATAFOLDER + "/config_params.txt") {
+    std::ifstream inFile(configFile);
+    if (!inFile.is_open()) {
+        std::cerr << "Error: Could not open configuration file for reading: " << configFile << std::endl;
+        return {8, 65537, 128}; // Return default values
+    }
     
-//     int depth = 8;      // Default value
-//     int modulus = 65537; // Default value
-//     int security = 128;  // Default value
+    int depth = 8;      // Default value
+    int modulus = 65537; // Default value
+    int security = 128;  // Default value
     
-//     std::string line;
-//     while (std::getline(inFile, line)) {
-//         std::istringstream iss(line);
-//         std::string key;
-//         std::getline(iss, key, '=');
+    std::string line;
+    while (std::getline(inFile, line)) {
+        std::istringstream iss(line);
+        std::string key;
+        std::getline(iss, key, '=');
         
-//         if (key == "depth") {
-//             iss >> depth;
-//         } else if (key == "modulus") {
-//             iss >> modulus;
-//         } else if (key == "security") {
-//             iss >> security;
-//         }
-//     }
+        if (key == "depth") {
+            iss >> depth;
+        } else if (key == "modulus") {
+            iss >> modulus;
+        } else if (key == "security") {
+            iss >> security;
+        }
+    }
     
-//     inFile.close();
-//     return {depth, modulus, security};
-// }
+    inFile.close();
+    return {depth, modulus, security};
+}
 
 
 
@@ -63,8 +63,75 @@ const std::string CRYPTOCONTEXT = "cryptocontext";
 
 int main()
 {
-    //auto [depth, modulus, security] = loadConfigParameters();
+    auto [depth, modulus, security] = loadConfigParameters();
 	
+    // 16, 512, 2, 8192, 2, 2, 3)
+    int p1 = 16;
+    int p2 = 512;
+    int p3 = 2;
+    int p4 = 8192;
+    int p5 = 2;
+    int p6 = 2;
+    int p7 = 3;
+
+    if (depth == 1) {
+        std::cerr << "using default configuration for GPU-1: " 
+            << p1 << ", " << p2 << ", " << p3 << ", "
+            << p4 << ", " << p5 << ", " << p6 << ", " << p7 << std::endl;
+
+    } else if (depth > 2 && depth <= 5) {
+        p1 = 32;
+        p2 = 512;
+        p3 = 6;
+        p4 = 16384;
+        p5 = 2;
+        p6 = 6;
+        p7 = 7;
+        std::cerr << "using configuration for GPU-5: " 
+            << p1 << ", " << p2 << ", " << p3 << ", "
+            << p4 << ", " << p5 << ", " << p6 << ", " << p7 << std::endl;
+
+    } else if (depth > 5 && depth <= 12) {
+        p1 = 64;
+        p2 = 512;
+        p3 = 25;
+        p4 = 32768;
+        p5 = 4;
+        p6 = 13;
+        p7 = 14;
+        std::cerr << "using configuration for GPU-12: " 
+            << p1 << ", " << p2 << ", " << p3 << ", "
+            << p4 << ", " << p5 << ", " << p6 << ", " << p7 << std::endl;
+    } else if (depth > 12 && depth <= 24) {
+        p1 = 128;
+        p2 = 512;
+        p3 = 25;
+        p4 = 65536;
+        p5 = 7;
+        p6 = 25;
+        p7 = 26;
+        std::cerr << "using configuration for GPU-24: " 
+            << p1 << ", " << p2 << ", " << p3 << ", "
+            << p4 << ", " << p5 << ", " << p6 << ", " << p7 << std::endl;
+
+    } else if (depth > 24 && depth <= 48) {
+        p1 = 128;
+        p2 = 512;
+        p3 = 50;
+        p4 = 65536;
+        p5 = 12;
+        p6 = 49;
+        p7 = 50;
+        std::cerr << "using configuration for GPU-48: " 
+            << p1 << ", " << p2 << ", " << p3 << ", "
+            << p4 << ", " << p5 << ", " << p6 << ", " << p7 << std::endl;
+
+    } else {
+        std::cerr << "Error: Unsupported depth value. Please use a depth between 1 and 10." 
+            << std::endl;
+        return 0;
+
+    }
     //getting the depth
 	//int depth = calculateDepth(DATAFOLDER);
 	//int depth = atoi(argv[1]);
@@ -76,7 +143,7 @@ int main()
 	cudaDataUtils& cudaUtils = cudaDataUtils::getInstance();
 	// Set GPU configuration - Note: suitable for T4 in AzureVM
 	// ringDim = 32768, sizeP = 3, sizeQ = 9, PHatModq_size_y = 10
-	cudaUtils.initialize(64, 512, 10, 32768, 3, 9, 10);
+	cudaUtils.initialize(p1, p2, p3, p4, p5, p6, p7);
 	#endif
 
 
